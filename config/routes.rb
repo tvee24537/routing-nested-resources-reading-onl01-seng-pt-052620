3377e4c2caf9e44b3fab669685c6e4819e011a44
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-21 20:02:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
-
+  get 'authors/:id/posts'
+  get 'authors/:id/posts/:post_id'
 end
